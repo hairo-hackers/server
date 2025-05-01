ec5ebf59dba9a4ad2eec4b0cffd755209a18ab6e
Hendrik Ebbers <hendrik.ebbers@web.de>
2025-05-01 09:32:04 -0700

    github token

--- src/main/java/org/hairo/server/github/webhook/GitHubWebhookEventTypes.java
+++ src/main/java/org/hairo/server/github/webhook/GitHubWebhookEventTypes.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 public enum GitHubWebhookEventTypes {
 
     PR_CREATED("pull_request", "opened"),
-    ISSUE_CREATED("issue", "opened"),
+    ISSUE_CREATED("issues", "opened"),
     ISSUE_COMMENT_CREATED("issue_comment", "created"),
     DISCUSSION_CREATED("discussion", "created"),
     DISCUSSION_COMMENT_CREATED("discussion_comment", "created");
